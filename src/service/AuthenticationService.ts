35a0a06400908d051c361baee2d68cb4e76f0495
Jeremy Hull <jchull@gmail.com>
2019-09-07 18:34:45 -0600

    auth service switch to use modules and config service, update token handling

--- src/service/AuthenticationService.ts
+++ src/service/AuthenticationService.ts
@@ -1,70 +1,100 @@
-import axios from 'axios';
+import axios, {AxiosInstance} from 'axios';
 
+import {ConfigurationService} from '@service/ConfigurationService';
 import decode from 'jwt-decode';
 
-// TODO: get from config
-const ENDPOINT = 'http://localhost:3001/login';
+const configurationService = new ConfigurationService();
 
 
 export class AuthenticationService {
-    endpoint: string;
+  private readonly endpoint: string;
+  private readonly api: AxiosInstance;
 
-    constructor(endpoint: string = ENDPOINT) {
-        this.endpoint = endpoint; // API server domain
-    // this.fetch = this.fetch.bind(this); // React binding stuff
-    // this.login = this.login.bind(this);
-    // this.getProfile = this.getProfile.bind(this);
-    }
+  constructor() {
+    this.endpoint = configurationService.get('REACT_APP_API_ROOT') || '/';
+    this.api = axios.create({
+      withCredentials: true,
+      baseURL: this.endpoint,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Access-Token': JSON.stringify(sessionStorage.getItem('access_token'))
+      }
+    });
 
-    async login(username: string, password: string): Promise<boolean>  {
-        const response = await axios.post(this.endpoint, {username,password});
-        if(response && response.hasOwnProperty('token')){
-            // @ts-ignore
-            const token = response.token;
-            this.setToken(token);
-            return true;
-        }
-        return false;
-    }
+    // this.api.interceptors.response.use(
+    //     response => response,
+    //     error => {
+    //       const originalRequest = error.config;
+    //
+    //       if (error.response.status === 401) {
+    //         const refresh_token = localStorage.getItem('refresh_token');
+    //
+    //         return this.api
+    //                    .post('/access-tokens/refresh', {refresh_token}, {})
+    //                    .then(({data}) => {
+    //                      localStorage.setItem('access_token', data.jwt);
+    //
+    //                      this.api.defaults.headers['X-Access-Token'] = data.jwt;
+    //                      originalRequest.headers['X-Access-Token'] = data.jwt;
+    //
+    //                      return axios(originalRequest);
+    //                    })
+    //                    .catch(err => {
+    //                      console.error(err)
+    //                      ;
+    //                    });
+    //       }
+    //
+    //       return Promise.reject(error);
+    //     }
+    // );
+  }
 
-    loggedIn(): boolean {
-        return this.isTokenValid(this.getToken());
+  async login(username: string, password: string): Promise<boolean> {
+    const response = await this.api.post('/login',
+        `username=${username}&password=${password}`,
+        {headers: {'Content-Type': 'application/x-www-form-urlencoded'}});
+    if (response && response.hasOwnProperty('token')) {
+      // @ts-ignore
+      const token = response.token;
+      this.setToken(token);
+      return true;
     }
+    return false;
+  }
 
-    isTokenValid(token: string | null): boolean {
-        if(!token)
-            return false;
-        try {
-            const decoded = decode(token);
-            // @ts-ignore
-            if (decoded.exp < Date.now() / 1000) { // Checking if token is expired. N
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            return false;
-        }
-    }
+  loggedIn(): boolean {
+    return this.isTokenValid(this.getToken());
+  }
 
-    setToken(idToken: string) {
-    // Saves user token to localStorage
-        localStorage.setItem('id_token', idToken);
+  isTokenValid(token: string | null): boolean {
+    if (!token) {
+      return false;
     }
+    try {
+      const decoded = decode(token);
+      // @ts-ignore
+      return decoded.exp < Date.now() / 1000;
+    } catch (err) {
+      return false;
+    }
+  }
 
-    getToken(): string | null {
-    // Retrieves the user token from localStorage
-        return localStorage.getItem('id_token');
-    }
+  setToken(idToken: string) {
+    sessionStorage.setItem('access_token', idToken);
+  }
 
-    logout() {
-    // Clear user token and profile data from localStorage
-        localStorage.removeItem('id_token');
-    }
+  getToken(): string | null {
+    return sessionStorage.getItem('access_token');
+  }
 
-    getProfile() {
-        const token = this.getToken();
-        return token && decode(token);
-    }
+  logout() {
+    sessionStorage.removeItem('access_token');
+  }
+
+
 
 }
+
+
