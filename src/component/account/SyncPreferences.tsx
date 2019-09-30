9c320e6424209046917a8c83760cd8806fa3593d
Jeremy Hull <jchull@gmail.com>
2019-09-30 10:32:38 -0600

    sync prefs property added

--- src/component/account/SyncPreferences.tsx
+++ src/component/account/SyncPreferences.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, FC, SyntheticEvent, useState} from 'react';
-import {DEFAULT_SYNC_PREFERENCES, ISyncPreferences, IVehicle} from 'tesla-dashboard-api';
+import {DEFAULT_SYNC_PREFERENCES, ISyncPreferences} from 'tesla-dashboard-api';
 import {userService} from '@service/Services';
 
 
@@ -22,6 +22,7 @@ export const SyncPreferences: FC<SyncPreferencesState> = props => {
       driving_minDurationMinutes,
       charging_maxGapSeconds,
       driving_maxGapSeconds,
+      sleepTriggerSeconds,
       charging_minDurationMinutes,
       charging_costPerKwhHome,
       charging_costPerKwhSupercharging,
@@ -35,6 +36,7 @@ export const SyncPreferences: FC<SyncPreferencesState> = props => {
       driving_minDurationMinutes,
       charging_maxGapSeconds,
       driving_maxGapSeconds,
+      sleepTriggerSeconds,
       charging_minDurationMinutes,
       charging_costPerKwhHome,
       charging_costPerKwhSupercharging,
@@ -59,87 +61,87 @@ export const SyncPreferences: FC<SyncPreferencesState> = props => {
   return (
       <div className="centered">
         <form onSubmit={handleSubmit}>
-        <section>
-          <label htmlFor="enabled">Enabled</label>
-          <input type="checkbox"
-                 name="enabled"
-                 checked={preferences.enabled}
-                 onChange={toggleEnabled}/>
-        </section>
-        <section>
-          <h3>Charging Preferences</h3>
-          <label htmlFor="charging_costPerKwhHome">Home charging cost</label>
-          <input
-              placeholder="Home Charging Cost"
-              name="charging_costPerKwhHome"
-              type="text"
-              value={preferences.charging_costPerKwhHome}
-              onChange={handleChange}
-          />
-          <label htmlFor="charging_costPerKwhSupercharging">Supercharging Cost</label>
-          <input
-              placeholder="Supercharging Cost"
-              name="charging_costPerKwhSupercharging"
-              type="text"
-              value={preferences.charging_costPerKwhSupercharging}
-              onChange={handleChange}
-          />
-          <label htmlFor="charging_minDurationMinutes">Minimum Charging Duration</label>
-          <input
-              placeholder="Minimum Charging Duration"
-              name="charging_minDurationMinutes"
-              type="text"
-              value={preferences.charging_minDurationMinutes}
-              onChange={handleChange}
-          />
-          <label htmlFor="charging_pollingIntervalsSeconds1">Level 1 Polling Interval</label>
-          <input
-              placeholder="Level 1 Polling Interval"
-              name="charging_pollingIntervalsSeconds1"
-              type="text"
-              value={preferences.charging_pollingIntervalsSeconds[0]}
-              // onChange={handleChange}
-              readOnly
-          />
-          <label htmlFor="charging_pollingIntervalsSeconds2">Level 2 Polling Interval</label>
-          <input
-              placeholder="Level 2 Polling Interval"
-              name="charging_pollingIntervalsSeconds2"
-              type="text"
-              value={preferences.charging_pollingIntervalsSeconds[1]}
-              // onChange={handleChange}
-              readOnly
-          />
-          <label htmlFor="charging_pollingIntervalsSeconds3">Level 3 Polling Interval</label>
-          <input
-              placeholder="Level 3 Polling Interval"
-              name="charging_pollingIntervalsSeconds3"
-              type="text"
-              value={preferences.charging_pollingIntervalsSeconds[2]}
-              // onChange={handleChange}
-              readOnly
-          />
-        </section>
+          <section>
+            <label htmlFor="enabled">Enabled</label>
+            <input type="checkbox"
+                   name="enabled"
+                   checked={preferences.enabled}
+                   onChange={toggleEnabled}/>
+          </section>
+          <section>
+            <h3>Charging Preferences</h3>
+            <label htmlFor="charging_costPerKwhHome">Home charging cost</label>
+            <input
+                placeholder="Home Charging Cost"
+                name="charging_costPerKwhHome"
+                type="text"
+                value={preferences.charging_costPerKwhHome}
+                onChange={handleChange}
+            />
+            <label htmlFor="charging_costPerKwhSupercharging">Supercharging Cost</label>
+            <input
+                placeholder="Supercharging Cost"
+                name="charging_costPerKwhSupercharging"
+                type="text"
+                value={preferences.charging_costPerKwhSupercharging}
+                onChange={handleChange}
+            />
+            <label htmlFor="charging_minDurationMinutes">Minimum Charging Duration</label>
+            <input
+                placeholder="Minimum Charging Duration"
+                name="charging_minDurationMinutes"
+                type="text"
+                value={preferences.charging_minDurationMinutes}
+                onChange={handleChange}
+            />
+            <label htmlFor="charging_pollingIntervalsSeconds1">Level 1 Polling Interval</label>
+            <input
+                placeholder="Level 1 Polling Interval"
+                name="charging_pollingIntervalsSeconds1"
+                type="text"
+                value={preferences.charging_pollingIntervalsSeconds[0]}
+                // onChange={handleChange}
+                readOnly
+            />
+            <label htmlFor="charging_pollingIntervalsSeconds2">Level 2 Polling Interval</label>
+            <input
+                placeholder="Level 2 Polling Interval"
+                name="charging_pollingIntervalsSeconds2"
+                type="text"
+                value={preferences.charging_pollingIntervalsSeconds[1]}
+                // onChange={handleChange}
+                readOnly
+            />
+            <label htmlFor="charging_pollingIntervalsSeconds3">Level 3 Polling Interval</label>
+            <input
+                placeholder="Level 3 Polling Interval"
+                name="charging_pollingIntervalsSeconds3"
+                type="text"
+                value={preferences.charging_pollingIntervalsSeconds[2]}
+                // onChange={handleChange}
+                readOnly
+            />
+          </section>
 
-        <section>
-          <h3>Driving Preferences</h3>
-          <label htmlFor="driving_minDurationMinutes">Minimum Driving Duration</label>
-          <input
-              placeholder="Minimum Driving Duration"
-              name="driving_minDurationMinutes"
-              type="text"
-              value={preferences.driving_minDurationMinutes}
-              onChange={handleChange}
-          />
-          <label htmlFor="driving_pollingIntervalSeconds">Polling Interval</label>
-          <input
-              placeholder="Polling Interval"
-              name="driving_pollingIntervalSeconds"
-              type="text"
-              value={preferences.driving_pollingIntervalSeconds}
-              onChange={handleChange}
-          />
-        </section>
+          <section>
+            <h3>Driving Preferences</h3>
+            <label htmlFor="driving_minDurationMinutes">Minimum Driving Duration</label>
+            <input
+                placeholder="Minimum Driving Duration"
+                name="driving_minDurationMinutes"
+                type="text"
+                value={preferences.driving_minDurationMinutes}
+                onChange={handleChange}
+            />
+            <label htmlFor="driving_pollingIntervalSeconds">Polling Interval</label>
+            <input
+                placeholder="Polling Interval"
+                name="driving_pollingIntervalSeconds"
+                type="text"
+                value={preferences.driving_pollingIntervalSeconds}
+                onChange={handleChange}
+            />
+          </section>
 
           <div>
             <button value="SUBMIT"
