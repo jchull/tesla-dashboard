57bb622091cfd818bea51d2d97dbbca4d2198134
jchull <jchull@gmail.com>
2019-11-26 10:12:53 -0700

    convert to use redux-toolkit to create reducer

--- src/component/session/reducer.ts
+++ src/component/session/reducer.ts
@@ -1,52 +1,29 @@
-import {SessionListAction, SessionListActionType, SessionState} from './actions';
+import {SessionState} from './actions';
 import {IVehicleSession} from 'tesla-dashboard-api';
+import {createReducer} from '@reduxjs/toolkit';
 
-const initialState = {
+const initialState: SessionState = {
   sessions: [] as IVehicleSession[]
 };
 
-export function sessionListReducer(state = initialState, action: SessionListAction): SessionState {
-  switch (action.type) {
-    case SessionListActionType.FETCH_SESSION_DETAILS__START:
-      return {
-        ...state
-      };
-    case SessionListActionType.FETCH_SESSION_DETAILS__FAIL:
-      return {
-        ...state
-        // message: "failed to get session details"
-      };
-    case SessionListActionType.FETCH_SESSION_DETAILS__SUCCESS:
-      return {
-        ...state,
-        selectedSessionStates: action.payload && action.payload.selectedSessionStates
-
-      };
-    case SessionListActionType.SELECT_SESSION:
-      return {
-        ...state,
-        selectedSessionId: action.payload && action.payload.selectedSessionId
-      };
-    case SessionListActionType.FETCH_SESSION_LIST__START:
-      return {
-        ...state
-      };
-
-    case SessionListActionType.FETCH_SESSION_LIST__SUCCESS:
-      return {
-        ...state,
-        sessions: (action.payload ? action.payload.sessions : []) as IVehicleSession[],
-        selectedSessionId: action.payload && action.payload.selectedSessionId
-      };
-
-    case SessionListActionType.FETCH_SESSION_LIST__FAIL:
-      return {
-        ...state
-        //TODO: add failure message
-      };
-
-
-    default:
-      return state;
+export const sessionListReducer = createReducer(initialState, {
+  FETCH_SESSION_DETAILS__START: (state, action) => {
+  },
+  FETCH_SESSION_DETAILS__FAIL: (state, action) => {
+  },
+  FETCH_SESSION_DETAILS__SUCCESS: (state, action) => {
+    state.selectedSessionStates = action.payload && action.payload.selectedSessionStates;
+  },
+  SELECT_SESSION: (state, action) => {
+    state.selectedSessionId = action.payload && action.payload.selectedSessionId;
+  },
+  FETCH_SESSION_LIST__START: (state, action) => {
+  },
+  FETCH_SESSION_LIST__SUCCESS: (state, action) => {
+    state.sessions = (action.payload ? action.payload.sessions : []) as IVehicleSession[];
+    state.selectedSessionId = action.payload && action.payload.selectedSessionId;
+  },
+  FETCH_SESSION_LIST__FAIL: (state, action) => {
+    //TODO: add failure message
   }
-}
+});
