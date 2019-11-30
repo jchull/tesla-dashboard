87c1357c3a1f257dfbe10082b46bde11f455bd7b
jchull <jchull@gmail.com>
2019-11-30 10:38:10 -0700

    add reducer and actions for auth state

--- src/auth/actions.ts
+++ src/auth/actions.ts
@@ -1,57 +1,61 @@
 import {ApiType} from '@service/index';
+import {createAction} from '@reduxjs/toolkit';
 
 
 export interface AuthState {
-  username: string,
-  loggedIn: boolean
+  username?: string,
+  loggedIn: boolean,
+  role?: string,
+  message?: string
 }
 
 export enum AuthActionType {
-  AUTH_LOGOUT__START = 'AUTH_LOGOUT__START'
+  AUTH_LOGIN__START = 'AUTH_LOGIN__START',
+  AUTH_LOGIN__FAILURE = 'AUTH_LOGIN__FAILURE',
+  AUTH_LOGIN__SUCCESS = 'AUTH_LOGIN__SUCCESS',
+  AUTH_LOGOUT__START = 'AUTH_LOGOUT__START',
+  AUTH_LOGOUT__SUCCESS = 'AUTH_LOGOUT__START',
 }
 
 export interface AuthAction {
   type: AuthActionType,
   payload?: {
-
+    username?: string,
+    password?: string
   }
 }
 
+export const authLoginStart = createAction(AuthActionType.AUTH_LOGIN__START);
+export const authLoginFail = createAction(AuthActionType.AUTH_LOGIN__FAILURE);
+export const authLoginSuccess = createAction(AuthActionType.AUTH_LOGIN__SUCCESS, (principal: { username: string, role: string }) => ({
+  payload: {
+    username: principal.username,
+    role: principal.role
+  }
+}));
 
-export function authLogout(): AuthAction {
-  return {
-    type: AuthActionType.AUTH_LOGOUT__START
-  };
-}
-//
-// export function fetchProductListFail(): ProductListAction {
-//   return {
-//     type: ProductListActionType.FETCH_PRODUCT_LIST__FAIL,
-//     payload: {
-//       message: 'failed to fetch product list'
-//     }
-//   };
-// }
-//
-// export function fetchProductListSuccess(productList: IVehicle[]): ProductListAction {
-//   return {
-//     type: ProductListActionType.FETCH_PRODUCT_LIST__SUCCESS,
-//     payload: {
-//       productList
-//     }
-//   };
-// }
+export const authLogoutStart = createAction(AuthActionType.AUTH_LOGOUT__START);
+export const authLogoutSuccess = createAction(AuthActionType.AUTH_LOGOUT__SUCCESS);
 
 
+export const loginAction =
+    (username: string, password: string) => async (dispatch: any, getState: any, extraArgument: { api: ApiType }): Promise<any> => {
+      dispatch(authLoginStart());
+      return extraArgument.api.auth.login(username, password)
+                          .then((result: boolean) => {
+                            const role = 'temp';
+                            if (result) {
+                              return dispatch(authLoginSuccess({username, role}));
+                            }
+                          })
+                          .catch(() => dispatch(authLoginFail()));
+    };
 
-// export const fetchProductListAction =
-//     () => async (dispatch: any, getState: any, extraArgument: { api: ApiType }): Promise<any> => {
-//       dispatch(fetchProductListStart());
-//       return extraArgument.api.queryService.getProducts()
-//                           .then((result: IVehicle[]) => {
-//                             dispatch(fetchProductListSuccess(result));
-//                           })
-//                           .catch(() => dispatch(fetchProductListFail()));
-//     };
-//
+export const logoutAction =
+    () => async (dispatch: any, getState: any, extraArgument: { api: ApiType }): Promise<any> => {
+      dispatch(authLogoutStart());
+      return extraArgument.api.auth.logout()
+                          .then(() => dispatch(authLogoutSuccess()));
+    };
+
 
--- src/auth/reducer.ts
+++ src/auth/reducer.ts
@@ -1,16 +1,22 @@
-import {AuthAction, AuthActionType, AuthState} from './actions';
+import {AuthActionType, AuthState} from './actions';
+import {createReducer} from '@reduxjs/toolkit';
 
 const initialState: AuthState = {
   username: '',
   loggedIn: false
 };
 
-export function authReducer(state = initialState, action: AuthAction): AuthState {
-  switch (action.type) {
-    case AuthActionType.AUTH_LOGOUT__START:
-      return initialState;
-
-    default:
-      return state;
+export const authReducer = createReducer(initialState, {
+  [AuthActionType.AUTH_LOGOUT__START]: (state, action) => {
+  },
+  [AuthActionType.AUTH_LOGOUT__SUCCESS]: (state, action) => {
+  },
+  [AuthActionType.AUTH_LOGIN__START]: (state, action) => {
+  },
+  [AuthActionType.AUTH_LOGIN__FAILURE]: (state, action) => {
+  },
+  [AuthActionType.AUTH_LOGIN__SUCCESS]: (state, action) => {
+    state.username = action.payload.username;
+    state.role = action.payload.role;
   }
-}
+});
