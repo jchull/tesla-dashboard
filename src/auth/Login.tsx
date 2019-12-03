a022654220c8269ea3880ab7e097b59aef5a2802
jchull <jchull@gmail.com>
2019-12-03 09:44:50 -0700

    migrate to redux

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,16 +1,21 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {Redirect} from 'react-router';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppState} from '@store/store';
 import {loginAction} from './actions';
+import {useHistory, useLocation} from 'react-router-dom';
 
 
 export const LoginComponent = () => {
 
   const [credentials, setCredentials] = useState({username: '', password: ''});
-  const loggedIn = useSelector((store:AppState) => store.auth.loggedIn);
-  const message = useSelector((store:AppState) => store.auth.message);
+  const loggedIn = useSelector((store: AppState) => store.auth.loggedIn);
+  const message = useSelector((store: AppState) => store.auth.message);
   const dispatch = useDispatch();
+
+  const history = useHistory();
+  const location = useLocation();
+  let {from} = location.state || {from: {pathname: '/'}};
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const {username, password} = Object.assign({}, credentials, {[event.target.name]: event.target.value});
@@ -20,48 +25,55 @@ export const LoginComponent = () => {
   async function handleSubmit(event: any) {
     event.preventDefault();
     const {username, password} = credentials;
-    dispatch(loginAction(username,password));
+    dispatch(loginAction(username, password));
+
   }
+
+  useEffect(() => {
+    if (loggedIn) {
+      history.replace(from);
+    }
+  }, [loggedIn]);
 
   return (
       <div>
         {loggedIn ?
-            <Redirect to="/"/>
-            :
-            <div className="centered">
-              <h3>Login</h3>
-              <form>
-                <input
-                    placeholder="Username"
-                    value={credentials.username}
-                    name="username"
-                    type="text"
-                    onChange={handleChange}
-                />
-                <input
-                    placeholder="Password"
-                    value={credentials.password}
-                    name="password"
-                    type="password"
-                    onChange={handleChange}
-                />
-                <input
-                    className="form-submit"
-                    value="SUBMIT"
-                    type="submit"
-                    onClick={handleSubmit}
-                />
-              </form>
-              <div className="links">
-                <a href='/signup'>Create Account</a>
-                <a href='/forgot'>Forgot Password</a>
-              </div>
+         <Redirect to={from}/>
+                  :
+         <div className="centered">
+           <h3>Login</h3>
+           <form>
+             <input
+                 placeholder="Username"
+                 value={credentials.username}
+                 name="username"
+                 type="text"
+                 onChange={handleChange}
+             />
+             <input
+                 placeholder="Password"
+                 value={credentials.password}
+                 name="password"
+                 type="password"
+                 onChange={handleChange}
+             />
+             <input
+                 className="form-submit"
+                 value="SUBMIT"
+                 type="submit"
+                 onClick={handleSubmit}
+             />
+           </form>
+           <div className="links">
+             <a href='/signup'>Create Account</a>
+             <a href='/forgot'>Forgot Password</a>
+           </div>
 
-            </div>
+         </div>
         }
         <div className="centered">
           {message &&
-          <h5>{message}</h5>}
+           <h5>{message}</h5>}
         </div>
       </div>
   );
