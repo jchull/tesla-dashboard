202f55197cdfdf599e793d9c4052d6adddf6d77e
jchull <jchull@gmail.com>
2020-01-08 13:18:04 -0700

    add JWT auth guard

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,11 +1,13 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, UseGuards} from '@nestjs/common';
 import {ProductService} from './product.service';
+import {AuthGuard} from '@nestjs/passport';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   getMyProducts() {
     return this.productService.getMyProducts('jchull');
