9bd16799a09c07f9e51056f37cd94345e57931f9
jchull <jchull@gmail.com>
2020-01-07 18:51:55 -0700

    add some product services

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
+import {ProductService} from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {
+  }
+
+  @Get()
+  getMyProducts() {
+    return this.productService.getMyProducts('jchull');
+  }
+}
--- server/src/product/product.module.ts
+++ server/src/product/product.module.ts
@@ -1,8 +1,24 @@
 import {Module} from '@nestjs/common';
 import {ProductController} from './product.controller';
 import {ProductService} from './product.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import {
+  ChargeSessionSchema,
+  ChargeStateSchema,
+  DriveSessionSchema, DriveStateSchema,
+  SyncPreferencesSchema,
+  VehicleSchema
+} from '../model';
 
 @Module({
+          imports: [
+              MongooseModule.forFeature([{name: 'Vehicle', schema: VehicleSchema}]),
+              MongooseModule.forFeature([{name: 'SyncPreferences', schema: SyncPreferencesSchema}]),
+              MongooseModule.forFeature([{name: 'ChargeSession', schema: ChargeSessionSchema}]),
+              MongooseModule.forFeature([{name: 'ChargeState', schema: ChargeStateSchema}]),
+              MongooseModule.forFeature([{name: 'DriveSession', schema: DriveSessionSchema}]),
+              MongooseModule.forFeature([{name: 'DriveState', schema: DriveStateSchema}])
+          ],
           controllers: [ProductController],
           providers: [ProductService]
         })
