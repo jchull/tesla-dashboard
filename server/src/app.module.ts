f0013341a91b18573cf2086c0be709b398fe088b
jchull <jchull@gmail.com>
2020-01-09 09:39:28 -0700

    static serve of react app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,33 +1,37 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {MongooseModule} from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-import {ConfigurationModule} from './configuration/configuration.module';
-import {ProductModule} from './product/product.module';
-import {AccountModule} from './account/account.module';
+import { ConfigurationModule } from './configuration/configuration.module';
+import { ProductModule } from './product/product.module';
+import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-          imports: [
-            ConfigModule.forRoot({
-                                   envFilePath: `./env/${process.env.NODE_ENV || 'development'}.env`
-                                 }),
-            MongooseModule.forRootAsync({
-                                          useFactory: async () => ({
-                                            uri: process.env.DB_CONN,
-                                            useNewUrlParser: true
-                                          })
-                                        }),
-            ConfigurationModule,
-            ProductModule,
-            AccountModule,
-            AuthModule
-          ],
-          controllers: [AppController],
-          providers: [AppService]
-        })
-export class AppModule {
-}
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./env/${process.env.NODE_ENV || 'development'}.env`,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.DB_CONN,
+        useNewUrlParser: true,
+      }),
+    }),
+    ConfigurationModule,
+    ProductModule,
+    AccountModule,
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../web', 'dist'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
