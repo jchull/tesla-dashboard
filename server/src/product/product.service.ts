baebfdf84b68cbeca66fd0cf3321e767137546f6
jchull <jchull@gmail.com>
2020-01-10 14:03:02 -0700

    implement getting recent sessions

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { VehicleType } from '../model';
+import {ChargeSessionType, DriveSessionType, VehicleType} from '../model';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectModel('Vehicle') private readonly productModel: Model<VehicleType>,
+    @InjectModel('DriveSession') private readonly driveSessionModel: Model<DriveSessionType>,
+    @InjectModel('ChargeSession') private readonly chargeSessionModel: Model<ChargeSessionType>
   ) {}
 
   async create(product: VehicleType) {
@@ -23,5 +25,23 @@ export class ProductService {
       .find({ username })
       .populate(['sync_preferences'])
       .sort({ $natural: -1 });
+  }
+
+  async findRecentSessions(username: string, productId: string, limit: number) {
+    const vehicle = await this.productModel.findOne({_id:productId});
+    const driveSessions = await this.driveSessionModel.find({vehicle})
+                                            .sort({$natural: -1})
+                                            .limit(limit)
+                                            .populate(['first', 'last', 'vehicle']);
+    const chargeSessions = await this.chargeSessionModel.find({vehicle})
+                                              .sort({$natural: -1})
+                                              .limit(limit)
+                                              .populate(['first', 'last', 'vehicle']);
+    const sessions = [...driveSessions, ...chargeSessions]
+        .sort((a: {start_date: number}, b: {start_date: number}) => b.start_date - a.start_date)// reverse sort
+        .slice(0, limit);
+    if (sessions.length) {
+      return sessions;
+    }
   }
 }
