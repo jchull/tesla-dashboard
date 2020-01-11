aa434e94456f1f5c1adc40f0836b104a4cc855cc
jchull <jchull@gmail.com>
2020-01-11 15:41:01 -0700

    add dependency

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,22 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {AuthService} from './auth/auth.service';
+import {AuthModule} from './auth/auth.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    process.env.JWT_SECRET = 'TEST_SECRET';
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AuthService]
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should ...', () => {
+
     });
   });
 });
