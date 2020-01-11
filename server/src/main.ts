94f9c18bdf3f6f01a660860fd4f9600365439d40
jchull <jchull@gmail.com>
2020-01-11 13:38:21 -0700

    added security packages

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(helmet());
+  // app.use(csurf());
+
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
