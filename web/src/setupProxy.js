68f4febfec652acb78cbfc2a2012ada12baa4223
jchull <jchull@gmail.com>
2020-01-11 13:39:28 -0700

    use 3001 for api port, this ensures that if the UI dev-server is used, it will proxy to a dev API server, not the one started with compose

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(proxy('/api', {
-        target: 'http://localhost:7057',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       })
   );
