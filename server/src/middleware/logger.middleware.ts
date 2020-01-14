9322d8c761cad3b68d059fa3af0ca64858f75755
jchull <jchull@gmail.com>
2020-01-14 14:38:33 -0700

    add params to logger middleware

--- server/src/middleware/logger.middleware.ts
+++ server/src/middleware/logger.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
-    console.log(`${req.method}: ${req.baseUrl} `);
+    console.log(`${req.method}: ${req.baseUrl} \t Query: ${JSON.stringify(req.query)}`);
     next();
   }
 }
