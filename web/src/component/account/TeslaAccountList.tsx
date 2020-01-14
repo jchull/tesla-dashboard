ebbaa842c60058fe0b71de20b6a28c271b40e76b
jchull <jchull@gmail.com>
2020-01-13 17:15:16 -0700

    new accounts should show as well

--- web/src/component/account/TeslaAccountList.tsx
+++ web/src/component/account/TeslaAccountList.tsx
@@ -43,7 +43,7 @@ export const TeslaAccountListComponent: FC = () => {
         }
 
         {
-          selectedAccount._id ?
+          selectedAccount ?
           <TeslaAccountComponent account={selectedAccount}/>
                           :
           <span>Select an Account</span>
