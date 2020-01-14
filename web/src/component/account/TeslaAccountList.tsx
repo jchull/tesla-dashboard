fb6fc64ada394be7b010f6915049872713461be4
jchull <jchull@gmail.com>
2020-01-14 11:41:03 -0700

    cleanup and use username from session

--- web/src/component/account/TeslaAccountList.tsx
+++ web/src/component/account/TeslaAccountList.tsx
@@ -1,53 +1,56 @@
-import React, { FC, useEffect, useState } from 'react';
-import { TeslaAccount } from '@model/index';
+import React, {FC, useEffect, useState} from 'react';
+import {TeslaAccount} from '@model/index';
 import services from '@service/service';
-import { TeslaAccountComponent } from '@component/account/TeslaAccount';
+import {TeslaAccountComponent} from '@component/account/TeslaAccount';
+import {AppState} from '@store/store';
+import {useSelector} from 'react-redux';
 
 export const TeslaAccountListComponent: FC = () => {
   const [accounts, setAccounts] = useState([] as TeslaAccount[]);
   const [selectedAccount, setSelectedAccount] = useState({} as TeslaAccount);
-  let username;
+  const username = useSelector((store: AppState) => store.auth.username);
 
   function addTeslaAccount() {
-    const newAccount = { email: '' };
-    setAccounts([newAccount, ...accounts] as TeslaAccount[]);
+    const newAccount = {email: '', username};
+    setAccounts([newAccount, ...accounts]);
     setSelectedAccount(newAccount);
   }
 
   useEffect(() => {
-    username = services.auth.getUsername();
     if (username) {
       services.userService
-        .getTeslaAccounts(username)
-        .then((data: TeslaAccount[] | undefined) => data && setAccounts(data));
+              .getTeslaAccounts(username)
+              .then((data: TeslaAccount[] | undefined) => data && setAccounts(data));
     }
   }, []);
 
   return (
-    <div>
-      <h3>Tesla Accounts</h3>
-      <a onClick={() => addTeslaAccount()}>Add Account</a>
-      {accounts && accounts.length ? (
-        <div>
-          {accounts.map((account) => (
-            <div
-              key={account.email}
-              className="clickable"
-              onClick={() => setSelectedAccount(account)}
-            >
-              {account.email || 'New Account'}
+      <div>
+        <h3>Tesla Accounts</h3>
+        <button onClick={() => addTeslaAccount()}>Link Tesla Account</button>
+        {accounts && accounts.length ? (
+            <div>
+              {accounts.map((account) => (
+                  <div
+                      key={account.email}
+                      className="clickable"
+                      onClick={() => setSelectedAccount(account)}
+                  >
+                    {account.email || 'New Account'}
+                  </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <span>No Accounts Configured</span>
-      )}
+        ) : (
+             <div className="messages">
+               <span>No Accounts Configured, <a onClick={() => addTeslaAccount()}>link a new Tesla account</a></span>
+             </div>
+         )}
 
-      {selectedAccount ? (
-        <TeslaAccountComponent account={selectedAccount} />
-      ) : (
-        <span>Select an Account</span>
-      )}
-    </div>
+        { selectedAccount.username ? (
+            <TeslaAccountComponent account={selectedAccount}/>
+        ) : (
+             accounts.length > 0 && <span>Select an Account</span>
+         )}
+      </div>
   );
 };
