70752f4c304eb46587e6bad21a744b24ea6e0d5e
Jeremy Hull <jchull@gmail.com>
2020-03-17 21:12:45 -0600

    add link for adding more linked energy accounts

--- packages/web/src/component/product/ProductList.tsx
+++ packages/web/src/component/product/ProductList.tsx
@@ -7,6 +7,7 @@ import { ProductListItem } from './ProductListItem';
 import { fetchProductListAction, ProductState, selectProduct } from './actions';
 
 import './product.style.scss';
+import {NavLink} from 'react-router-dom';
 
 export const ProductList: React.FC<ProductState> = (props: ProductState) => {
   const dispatch = useDispatch();
@@ -25,6 +26,7 @@ export const ProductList: React.FC<ProductState> = (props: ProductState) => {
           selected={props.selectedProductId === product._id}
         />
       ))}
+      <NavLink to="/tesla-account" className="add-more">add more linked accounts</NavLink>
     </div>
   );
 };
