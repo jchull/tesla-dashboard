9dcf548a4511b5343a86ef07ee7c925e951cf1f6
Jeremy Hull <jchull@gmail.com>
2020-03-17 21:10:08 -0600

    new panels

---
+++ packages/web/src/component/stats/StatsPanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface StatsState {
+
+}
+
+export const StatsPanel: React.FC<StatsState> = (props) => {
+
+  return (
+    <div className="card">
+      Stats
+    </div>
+  );
+};
