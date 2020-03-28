af4e70387cf327baab3888f234d6a197b310185a
Jeremy Hull <jchull@gmail.com>
2020-03-28 11:49:33 -0600

    Add some basic info to stats panel

--- packages/web/src/component/stats/StatsPanel.tsx
+++ packages/web/src/component/stats/StatsPanel.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {AppState} from '../../store';
 
 interface StatsState {
 
@@ -6,9 +8,20 @@ interface StatsState {
 
 export const StatsPanel: React.FC<StatsState> = (props) => {
 
+  const selectedSessionId = useSelector((store: AppState) =>
+      store.session.selectedSessionId);
+
+  const loadedCount = useSelector((store: AppState) =>
+      store.session.loadedCount);
+  const totalCount = useSelector((store: AppState) =>
+      store.session.totalCount);
+
+
   return (
     <div className="card">
-      Stats
+      <h5>Stats</h5>
+      <div>Selected Session: {selectedSessionId || '-'}</div>
+      <div>Paging: {loadedCount}/{totalCount}</div>
     </div>
   );
 };
