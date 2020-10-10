9659667665bbc560f183ddb1e9b5b5d453ef83cf
Jeremy Hull <jchull@gmail.com>
2020-10-10 11:40:32 -0600

    handle charge/drive query transparently

--- packages/server/src/session/session.service.ts
+++ packages/server/src/session/session.service.ts
@@ -1,6 +1,6 @@
-import {forwardRef, Inject, Injectable} from '@nestjs/common';
-import {InjectModel} from '@nestjs/mongoose';
-import {Model} from 'mongoose';
+import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 import {
   ChargeSession,
   ChargeSessionType,
@@ -8,12 +8,11 @@ import {
   DriveSession,
   DriveSessionType,
   DriveStateType,
-  Operator,
   QueryResult,
   QuerySet,
   VehicleType
-} from '@teslapp/common';
-import {ProductService} from '../product/product.service';
+} from '@teslapp/common'
+import { ProductService } from '../product/product.service'
 
 @Injectable()
 export class SessionService {
@@ -29,48 +28,49 @@ export class SessionService {
     private readonly chargeStateModel: Model<ChargeStateType>,
     @Inject(forwardRef(() => ProductService))
     private readonly productService: ProductService
-  ) {}
+  ) {
+  }
 
   async getSessionDetails(username: string, id: string) {
     // TODO: limit access by username
     const driveStates = await this.driveStateModel
-                                  .find({driveSession: id})
-                                  .sort({timestamp: 1});
+                                  .find({ driveSession: id })
+                                  .sort({ timestamp: 1 })
     if (driveStates.length) {
-      return driveStates;
+      return driveStates
     } else {
       const chargeStates = await this.chargeStateModel
-                                     .find({chargeSession: id})
-                                     .sort({timestamp: 1});
+                                     .find({ chargeSession: id })
+                                     .sort({ timestamp: 1 })
       if (chargeStates.length) {
-        return chargeStates;
+        return chargeStates
       }
     }
   }
 
   async deleteSession(username: string, id: string) {
     // TODO: limit access by username matching
-    const deleteCount = await this.driveSessionModel.deleteOne({_id: id});
+    const deleteCount = await this.driveSessionModel.deleteOne({ _id: id })
     if (deleteCount.deletedCount) {
       const deleteItemCount = await this.driveStateModel.deleteMany({
         driveSession: id
-      });
+      })
       if (deleteItemCount.deletedCount) {
         return (
-            (deleteCount.deletedCount || 0) + (deleteItemCount.deletedCount || 0)
-        );
+          (deleteCount.deletedCount || 0) + (deleteItemCount.deletedCount || 0)
+        )
       }
     } else {
-      const deleteCount = await this.chargeSessionModel.deleteOne({_id: id});
+      const deleteCount = await this.chargeSessionModel.deleteOne({ _id: id })
       if (deleteCount.deletedCount) {
         const deleteItemCount = await this.chargeStateModel.deleteMany({
           chargeSession: id
-        });
+        })
         if (deleteItemCount.deletedCount) {
           return (
-              (deleteCount.deletedCount || 0) +
-              (deleteItemCount.deletedCount || 0)
-          );
+            (deleteCount.deletedCount || 0) +
+            (deleteItemCount.deletedCount || 0)
+          )
         }
       }
     }
@@ -79,25 +79,25 @@ export class SessionService {
   async addTag(username: string, sessionId: string, tag: string) {
     const driveSession = await this.driveSessionModel.findOne({
       _id: sessionId
-    });
+    })
     if (driveSession && !driveSession.tags.includes(tag)) {
-      driveSession.tags.push(tag);
+      driveSession.tags.push(tag)
       const result = await this.driveSessionModel.updateOne(
-          {_id: sessionId},
-          driveSession
-      );
-      return result.tags;
+        { _id: sessionId },
+        driveSession
+      )
+      return result.tags
     } else {
       const chargeSession = await this.chargeSessionModel.findOne({
         _id: sessionId
-      });
+      })
       if (chargeSession && !chargeSession.tags.includes(tag)) {
-        chargeSession.tags.push(tag);
+        chargeSession.tags.push(tag)
         const result = await this.chargeSessionModel.updateOne(
-            {_id: sessionId},
-            chargeSession
-        );
-        return result.tags;
+          { _id: sessionId },
+          chargeSession
+        )
+        return result.tags
       }
     }
   }
@@ -105,75 +105,75 @@ export class SessionService {
   async removeTag(username: string, sessionId: string, tag: string) {
     const driveSession = await this.driveSessionModel.findOne({
       _id: sessionId
-    });
+    })
     if (driveSession && driveSession.tags.includes(tag)) {
-      driveSession.tags.splice(driveSession.tags.indexOf(tag), 1);
+      driveSession.tags.splice(driveSession.tags.indexOf(tag), 1)
       const result = await this.driveSessionModel.updateOne(
-          {_id: sessionId},
-          driveSession
-      );
-      return result.tags;
+        { _id: sessionId },
+        driveSession
+      )
+      return result.tags
     } else {
       const chargeSession = await this.chargeSessionModel.findOne({
         _id: sessionId
-      });
+      })
       if (chargeSession && chargeSession.tags.includes(tag)) {
-        chargeSession.tags.splice(chargeSession.tags.indexOf(tag), 1);
+        chargeSession.tags.splice(chargeSession.tags.indexOf(tag), 1)
         const result = await this.chargeSessionModel.updateOne(
-            {_id: sessionId},
-            chargeSession
-        );
-        return result.tags;
+          { _id: sessionId },
+          chargeSession
+        )
+        return result.tags
       }
     }
   }
 
   async findSessions(username: string, query: QuerySet): Promise<QueryResult> {
     const vehicleId = query.predicates.find((p) => p.field === 'vehicle')
-      ?.value;
+      ?.value
     if (!vehicleId) {
-      throw Error('vehicle/product required in session predicate');
+      throw Error('vehicle/product required in session predicate')
     }
-    const tags = query.predicates.filter((p) => p.field === 'tags');
-    //TODO: REMOVE temp limit to business tags
-   // tags.push({field: 'tags', value: 'business', operator: Operator.EQ});
-    const skip = query.page.start;
-    // TODO: handle charging later and organize this function better
-    if (query.type === 'drive') {
-      // set up criteria for count and query
-      const criteria = {
-        vehicle: { _id: vehicleId, username },
-        tags: tags.map((p) => p.value),
-        start_date: { $gte: jan2019, $lte: dec2019 }
-      };
+    const tags = query.predicates.filter((p) => p.field === 'tags')
 
-      // get a total count for pagination info
-      const countQuery = this.driveSessionModel.countDocuments();
-      countQuery.setQuery(criteria);
-      const count = await countQuery.exec();
+    const skip = query.page.start
+    // set up criteria for count and query
+    const criteria = {
+      vehicle: { _id: vehicleId, username },
+      tags: tags.map((p) => p.value)
+      // start_date: { $gte: jan2019, $lte: dec2019 }
+    }
+    const model = query.type.toLowerCase() === 'drive' ?
+      this.driveSessionModel : this.chargeSessionModel
 
-      // do query with same criteria as count
-      const mongooseQuery = this.driveSessionModel.find();
-      mongooseQuery.setQuery(criteria);
-      mongooseQuery.populate('first');
-      mongooseQuery.populate('last');
-      mongooseQuery.skip(query.page.start)
-                   .limit(query.page.size);
-      if (query.sort) {
-        const {field, desc} = query.sort[0];
-        mongooseQuery.sort(`${desc ? '-' : ''}${field}`);
-      }
-      const result = await mongooseQuery.exec();
+    // get a total count for pagination info
+    const countQuery = model.countDocuments()
+    countQuery.setQuery(criteria)
+    const count = await countQuery.exec()
 
-      return {
-        page: {size: result.length, start: skip, total: count},
-        results: result
-      };
+    // do query with same criteria as count
+    // @ts-ignore
+    const mongooseQuery = model.find()
+    mongooseQuery.setQuery(criteria)
+    mongooseQuery.populate('first')
+    mongooseQuery.populate('last')
+    mongooseQuery.skip(query.page.start)
+                 .limit(query.page.size)
+    if (query.sort) {
+      const { field, desc } = query.sort[0]
+      mongooseQuery.sort(`${desc ? '-' : ''}${field}`)
+    }
+    const result = await mongooseQuery.exec()
+
+    return {
+      page: { size: result.length, start: skip, total: count },
+      results: result
     }
   }
 
+
   async createNewSession() {
     // TODO
-    const session = {};
+    const session = {}
   }
 }
