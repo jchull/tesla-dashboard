ea99c065502daee51105d5dbac2305d64db3c22f
Jeremy Hull <jchull@gmail.com>
2020-11-28 10:47:13 -0700

    cleanup sync service

--- packages/server/src/data-sync/tesla-sync/tesla-sync.service.ts
+++ packages/server/src/data-sync/tesla-sync/tesla-sync.service.ts
@@ -1,5 +1,5 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common'
-import { query, tesla, types } from '@teslapp/common'
+import { tesla, types } from '@teslapp/common'
 import { TeslaAccountService } from '../../tesla-account/tesla-account.service'
 import { TeslaOwnerService } from '../../tesla-account/tesla-owner/tesla-owner.service'
 import { ProductService } from '../../product/product.service'
@@ -32,6 +32,12 @@ export class TeslaSyncService {
           product.id_s
         )
 
+        product.odometer = vehicleData.vehicle_state.odometer
+        product.charge_limit_soc = vehicleData.charge_state.charge_limit_soc
+        product.battery_level = vehicleData.charge_state.battery_level
+        product.charging_state = vehicleData.charge_state.charging_state
+        product.timestamp = vehicleData.vehicle_state.timestamp
+
         // TODO: get from sync preferences
         const activityTimeoutSeconds = 300 // 5 minutes
 
@@ -39,22 +45,8 @@ export class TeslaSyncService {
           const vehicleStatus = this.findVehicleState(vehicleData)
           console.log(`${vehicleData.display_name} is currently ${vehicleStatus}`)
 
-          // TODO: add time to query so we only find active session, sort by time descending
-          const activeSession = (
-            await this.sessionService.findActivities(product.username, {
-              predicates: [
-                { operator: query.Operator.EQ, value: product, field: 'vehicle' },
-                { operator: query.Operator.EQ, value: vehicleStatus, field: 'activity' },
-                {
-                  operator: query.Operator.GTE,
-                  value: vehicleData.vehicle_state.timestamp - activityTimeoutSeconds,
-                  field: 'end_date'
-                }
-              ],
-              page: { size: 1, start: 0 }
-            })
-          )?.results
-          return activeSession?.length === 1 ?
+          const activeSession = await this.sessionService.findCurrentActivity(product, vehicleStatus, vehicleData.vehicle_state.timestamp - activityTimeoutSeconds)
+          return activeSession ?
             this.sessionService.appendVehicleState(activeSession[0], vehicleData)
             :
             this.sessionService.createNewActivity(product, vehicleStatus, vehicleData)
@@ -67,6 +59,7 @@ export class TeslaSyncService {
 
   async syncVehiclesByAccount(username: string) {
     const accountList = await this.teslaAccountService.getAccounts(username)
+    console.log('TODO: implement upstream sync')
     // TODO
   }
 
@@ -90,7 +83,4 @@ export class TeslaSyncService {
         types.ActivityType.PARKED
   }
 
-  private isActive(vehicleData: tesla.VehicleData): boolean {
-    return true // TODO: logic ya
-  }
 }
