7213418f83a4efd4baa72464e74233c3e1f06457
Jeremy Hull <jchull@gmail.com>
2020-12-31 10:34:30 -0700

    Create basic tabs component

---
+++ packages/web/src/component/common/Tabs.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Tabs.scss'
+
+interface TabProps {
+  tabs: string[],
+  selectedIndex: number,
+
+  onSelect(index: number): any
+}
+
+export const Tabs: React.FC<TabProps> = (props) => {
+
+  function mapItemsToElements(items: string[], selectedIndex): JSX.Element[] {
+    return items.map((item, index) => (
+      <button key={item}
+              onClick={() => props.onSelect(index)}
+              className={index === selectedIndex ? 'selected' : ''}>
+        {item}
+      </button>
+    ))
+  }
+
+  const content = mapItemsToElements(props.tabs, props.selectedIndex)
+
+  return (
+    <div className='tab-buttons'>
+      {content}
+    </div>
+  )
+}
